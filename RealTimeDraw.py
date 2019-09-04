47ea19277591055251f334866e471680eb1908b9
Breanna Apple <bapple@uvm.edu>
2019-09-04 19:01:24 -0400

    created .py files

---
+++ RealTimeDraw.py
@@ -0,0 +1,8 @@
+from pygameWindow import PYGAME_WINDOW
+
+window = PYGAME_WINDOW()
+
+print(window)
+
+#while True:
+#	print(’Draw something.’)
---
+++ pygameWindow.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class PYGAME_WINDOW:
+	def init (self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((300,100))
+
+	def Prepare():
+		pass
+
+	def Reveal():
+		pass
