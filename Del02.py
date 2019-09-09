d1761829d447278d00727eb761f5de8bee8e8728
Breanna Apple <bapple@uvm.edu>
2019-09-09 14:10:50 -0400

    Deliverable 02

--- Del02.py
+++ Del02.py
@@ -18,56 +18,55 @@ yMax = -1000.0
 
 pygameWindow = PYGAME_WINDOW()
 
-print(pygameWindow)
-
-def Perturb_Circle_Position():
-	global x, y
-	fourSidedDieRoll = random.randint(1, 4)
-	if fourSidedDieRoll == 1:
-		x -= 1
-	elif fourSidedDieRoll == 2:
-		x += 1
-	elif fourSidedDieRoll == 3:
-		y -= 1
-	else:
-		y += 1
-
 def Scale(x, oldMin, oldMax, newMin, newMax):
 	if oldMin == oldMax:
 		newX = float(newMax - newMin)/2
 	else:
 		newX = float(newMin) + float(newMax - newMin) * float(x - oldMin)/float(oldMax - oldMin)
 
-
 	return newX
+
+def Handle_Vector_From_Leap(v):
+	global xMin,xMax,yMin,yMax
+	vX = int(v[0])
+	vY = int(v[1])
+
+	if vX < xMin:
+		xMin = vX
+	elif vX > xMax:
+		xMax = vX
+	if vY < yMin:
+		yMin = vY
+	elif vY > yMax:
+		yMax = vY
+
+	pygameX = Scale(vX, xMin, xMax, 0, pygameWindowWidth)
+	pygameY = Scale(vY, yMin, yMax, pygameWindowDepth, 0)
+		
+	return (pygameX,pygameY)
+
+
+def Handle_Bone(bone, b):
+	base = bone.prev_joint
+	tip = bone.next_joint
+	baseCoords = Handle_Vector_From_Leap(base)
+	tipCoords = Handle_Vector_From_Leap(tip)
+	pygameWindow.Draw_Black_Line(baseCoords, tipCoords, 4-b)
+
+def Handle_Finger(finger):
+	for b in range(0,3):
+		Handle_Bone(finger.bone(b), b)
 
 def Handle_Frame(frame):
 	global x, y, xMin, xMax, yMin, yMax
 	hand = frame.hands[0]
 	fingers = hand.fingers
-	indexFingerList = fingers.finger_type(Finger.TYPE_INDEX)
-	indexFinger = indexFingerList[0]
-	distalPhalanx = indexFinger.bone(3)
-	tip = distalPhalanx.next_joint
-	x = int(tip[0])
-	y = int(tip[1])
-	if x < xMin:
-		xMin = x
-	elif x > xMax:
-		xMax = x
-	if y < yMin:
-		yMin = y
-	elif y > yMax:
-		yMax = y
+	for finger in fingers:
+		Handle_Finger(finger)
 
 while True:
 	pygameWindow.Prepare()
 	frame = controller.frame()
 	if len(frame.hands) > 0:
 		Handle_Frame(frame)
-		pygameX = Scale(x, xMin, xMax, 0, pygameWindowWidth)
-		pygameY = Scale(y, yMin, yMax, pygameWindowDepth, 0)
-		pygameWindow.Draw_Black_Circle(int(pygameX), int(pygameY))
 	pygameWindow.Reveal()
-	"""Perturb_Circle_Position()
-	pygameWindow.Draw_Black_Circle(x,y)"""
--- pygameWindow.py
+++ pygameWindow.py
@@ -15,3 +15,6 @@ class PYGAME_WINDOW:
 
 	def Draw_Black_Circle(self, x, y):
 		pygame.draw.circle(self.screen, (0,0,0), (x,y), 20, 0)
+
+	def Draw_Black_Line(self, (xBase, yBase), (xTip, yTip), width):
+		pygame.draw.line(self.screen, (0,0,0), (xBase, yBase), (xTip, yTip), width)
